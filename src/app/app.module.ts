9135be5a45b9310b64dc84779e1217c2ca74332c
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-14 15:01:40 +0530

    signin and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+const addRoute :Routes = [
+  {
+    path:"",component:SigninComponent
+  },
+  {
+    path:"signup",component:SignupComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(addRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  name = ""
+  phone = ""
+  gender = ""
+  email = ""
+  address = ""
+  pincode = ""
+  bloodGroup = ""
+  dateOfBirth = ""
+  parentName = ""
+  age = ""
+  password = ""
+  confirmPassword = ""
+
+  getValues=()=>{
+    let data = {
+      "name":this.name,
+      "phone":this.phone,
+      "gender":this.gender,
+      "email":this.email,
+      "address":this.address,
+      "pincode":this.pincode,
+      "bloodGroup":this.bloodGroup,
+      "dateOfBirth":this.dateOfBirth,
+      "parentName":this.parentName,
+      "age":this.age,
+      "password":this.password,
+      "confirmPassword":this.confirmPassword,
+    }
+    console.log(data)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
